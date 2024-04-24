42e0fa95a7108c74319486246527a2b05316e95e
Parkku <qkzn326@gmail.com>
2024-04-24 13:10:35 +0900

    Update 01-DML-SELECT.sql
    오전 커밋

--- 01-DML-SELECT.sql
+++ 01-DML-SELECT.sql
@@ -51,3 +51,151 @@ FROM employees;
 -- 오류의 원인 : JOB_ID 는 문자열(VARCHAR2) 데이터 이기 때문에 산술연산 X
 
 DESC employees;
+
+-- NULL
+--이름, 급여 커미션 비율을 출력
+SELECT first_name, salary, commission_pct
+FROM employees;
+
+--이름과 커미션까지 포함한 급여를 출력해보자
+SELECT first_name, salary, commission_pct, salary * (1+commission_pct)
+FROM employees;
+
+--NULL이 포함된 연산식의 결과는 NULL
+--NULL을 처리하기 위한 함수 NVL이 필요함
+--NVL(표현식1, 표현식1이 널일 경우의 대체값)
+
+-- NVL 활용 대체값 계산
+SELECT first_name, salary, commission_pct, 
+    salary * (1 + NVL(commission_pct, 0))
+FROM employees;
+
+
+--Null은 0이나 ""과 다르게 빈 값이다.
+--Null은 산술연산 결과, 통계 결과를 왜곡시키기 때문에 -> NULL에 대한 처리는 철저히 할 것.
+
+--별칭 Alias
+--Projection 단계에서 출력용으로 표시되는 임시 `컬럼제목`
+
+-- 컬럼명 뒤에 별칭을 붙이거나 컬럼명 뒤에 as 키워드 별칭
+-- 표시명에 특수문자 포함된 경우 " " 묶어서 부여
+
+-- 직원아이디, 이름, 급여 출력
+-- 직원 아이디는 empNo, 이름은 f-name, 급여는 월 급으로 표시해라
+SELECT employee_id as empNo, 
+    first_name as "f-name",     -- 컬럼명 뒤에 as 별칭
+    salary "월 급" -- 컬럼명 뒤에 별칭
+From employees;
+
+
+-- 직원 이름(first_name last_name 합쳐서) name 이라고 출력
+-- 급여(커미션이 포함된 급여), 급여 * 12 연봉 별칭으로 표기
+SELECT first_name ||''|| last_name,
+    salary *(1 + nvl(commission_pct,0)) "급여(커미션포함)",
+    salary * 12 연봉
+FROM employees;
+
+--연습 : Alias 붙이기 (employees 테이블)
+--이름 : first_name last_name
+--입사일 : hire_date
+--전화번호 : phone_number
+--급여 : salary
+--연봉 : salary * 12
+
+SELECT first_name ||''|| last_name "이름",
+    hire_date 입사일,
+    phone_number 전화번호,
+    salary 급여,
+    salary * 12 연봉
+FROM employees;
+    
+
+----------------------
+-- WHERE
+----------------------
+-- 특정 조건을 기준으로 레코드를 선택(SELECTION)
+
+-- 비교 연산 : =, <>, >, >=, <, <=
+-- 사원들 중에서 급여가 15000 이상인 직원의 이름과 급여
+SELECT first_name, salary
+FROM employees
+WHERE salary > 15000;
+
+-- 입사일이 07/01/01 이후인 직원들의 이름과 입사일
+SELECT first_name, hire_date
+FROM employees
+WHERE hire_date >= '17/01/01';
+
+-- 급여가 14000 이하이거나, 17000 이상인 사원의 이름과 급여
+SELECT first_name, salary
+FROM employees
+WHERE salary <= 4000       -- 첫 번째
+OR salary >= 17000;     -- 두 번째 조건
+
+-- 급여가 14000 이상이고, 17000 이하인 사원의 이름과 급여
+SELECT first_name, salary
+FROM employees
+WHERE salary >= 14000       -- 첫 번째
+AND salary <= 17000;     -- 두 번째 조건
+
+-- BETWEEN : 범위 비교
+SELECT first_name, salary
+FROM employees
+WHERE salary BETWEEN 14000 AND 17000;
+
+--NULL 체크 =, <> 사용하면 안됨
+--IS NULL, IS NOT NULL
+
+--commission 을 받지 않는 사람들 (-> commission_pct 가 NULL인 레코드)
+SELECT first_name, commission_pct
+FROM employees
+WHERE commission_pct IS NULL;  -- NULL 체크
+
+--commission 을 받는 사람들 (-> commission_pct 가 NULL이 아닌 레코드)
+SELECT first_name, commission_pct
+FROM employees
+WHERE commission_pct IS NOT NULL;  -- NULL 체크
+
+
+-- 사원들 중 10, 20, 40번 부서에서 근무하는 직원들의 이름과 부서 아이디.
+SELECT first_name, department_id
+FROM employees
+WHERE department_id = 10 or
+    department_id = 20 or
+    department_id = 40;
+    
+-- IN 연산자 : 특정 집합의 요소와 비교
+SELECT first_name, department_id
+FROM employees
+WHERE department_id In (10, 20, 40);
+
+
+------------------
+--LIKE 연산
+------------------
+--와일드카드(%, _)를 이용한 부분 문자열 매핑
+--% : 0개 이상의 정해지지 않은 문자열
+--_ : 1개의 정해지지 않은 문자 
+
+-- 이름의 am을 포함하고 있는 사원의 이름과 급여 출력
+SELECT first_name, salary
+From employees
+WHERE LOWER(first_name) LIKE '%am%';   -- 이름을 소문자로 바꾸고 am 앞뒤에는 뭐가 오든 상관 않겠다.
+
+
+-- 이름의 두 번째 글자가 a인 사원의 이름과 급여
+SELECT first_name, salary
+From employees
+WHERE LOWER(first_name) LIKE '_a%'; 
+
+
+-- 이름의 네번째 글자가 a인 사원의 이름과 급여
+SELECT first_name, salary
+From employees
+WHERE LOWER(first_name) LIKE '___a%'; 
+
+
+-- 이름이 네 글자인 사원들 중에서 두 번째 글자가 a인 사원
+SELECT first_name, salary
+From employees
+WHERE LOWER(first_name) LIKE '____' and '_a%'; 
