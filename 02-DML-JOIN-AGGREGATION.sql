0b3dec7ad7641d758f62039a743141a2286a6b3f
Parkku <qkzn326@gmail.com>
2024-04-29 17:34:45 +0900

    2024 04 29 오후 커밋
    프랙티스 02, 집계함수 part2

--- 02-DML-JOIN-AGGREGATION.sql
+++ 02-DML-JOIN-AGGREGATION.sql
@@ -6,38 +6,45 @@
 describe emloyees;
 desc DEPARTMENTS;
 
-select * from EMPLOYEES; -- 107 rows
-select * from DEPARTMENTS; -- 27 rows
+select *
+from EMPLOYEES; -- 107 rows
+select *
+from DEPARTMENTS; -- 27 rows
 
 select *
-from EMPLOYEES, DEPARTMENTS;    -- 107 x 27
+from EMPLOYEES,
+     DEPARTMENTS;
+-- 107 x 27
 
 -- 카티전 프로덕트
 select *
-from EMPLOYEES,DEPARTMENTS
+from EMPLOYEES,
+     DEPARTMENTS
 where EMPLOYEES.DEPARTMENT_ID = DEPARTMENTS.DEPARTMENT_ID;
 
 -- INNER JOIN, EQUI-JOIN
 
 -- alias 를 이용한 원하는 필드의 Projection
 ----------------------------------------
-    -- simple Join or Equi-Join
+-- simple Join or Equi-Join
 ----------------------------------------
 
 select FIRST_NAME,
        dept.DEPARTMENT_ID,
        emp.DEPARTMENT_ID,
        DEPARTMENT_NAME
-from EMPLOYEES emp, DEPARTMENTS dept
-where emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;   -- 106명, department_id 가 null 인 직원은 JOIN 에서 배제
+from EMPLOYEES emp,
+     DEPARTMENTS dept
+where emp.DEPARTMENT_ID = dept.DEPARTMENT_ID; -- 106명, department_id 가 null 인 직원은 JOIN 에서 배제
 
 select *
 from EMPLOYEES
-where DEPARTMENT_ID is null;    -- 178 kimberely
+where DEPARTMENT_ID is null; -- 178 kimberely
 
 select emp.FIRST_NAME,
        dept.DEPARTMENT_NAME
-from EMPLOYEES emp join DEPARTMENTS dept using (DEPARTMENT_ID);
+from EMPLOYEES emp
+         join DEPARTMENTS dept using (DEPARTMENT_ID);
 
 -------------------
 -- Theta Join
@@ -50,7 +57,8 @@ select emp.EMPLOYEE_ID,
        emp.JOB_ID,
        j.JOB_ID,
        j.JOB_TITLE
-from EMPLOYEES emp join jobs j on emp.JOB_ID = j.JOB_ID
+from EMPLOYEES emp
+         join jobs j on emp.JOB_ID = j.JOB_ID
 where emp.SALARY <= (j.MIN_SALARY + j.MAX_SALARY) / 2;
 
 
@@ -70,11 +78,14 @@ select emp.FIRST_NAME,
        emp.DEPARTMENT_ID,
        dept.DEPARTMENT_ID,
        DEPARTMENT_NAME
-from EMPLOYEES emp, DEPARTMENTS dept
+from EMPLOYEES emp,
+     DEPARTMENTS dept
 where emp.DEPARTMENT_ID = dept.DEPARTMENT_ID (+); -- null 이 포함된 테이블 쪽에 (+) 표기
 
 select *
-from EMPLOYEES where DEPARTMENT_ID is null; -- kimberely -> 부서에 소속되지 않음
+from EMPLOYEES
+where DEPARTMENT_ID is null;
+-- kimberely -> 부서에 소속되지 않음
 
 
 -- ANSI SQL - 명시적으로 JOIN 방법을 정한다
@@ -83,8 +94,8 @@ select FIRST_NAME,
        dept.DEPARTMENT_ID,
        DEPARTMENT_NAME
 from EMPLOYEES emp
-    left outer join DEPARTMENTS dept
-        on emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;
+         left outer join DEPARTMENTS dept
+                         on emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;
 
 --------------------
 -- right OUTER JOIN
@@ -96,8 +107,10 @@ select FIRST_NAME,
        emp.DEPARTMENT_ID,
        dept.DEPARTMENT_ID,
        DEPARTMENT_NAME
-from EMPLOYEES emp, DEPARTMENTS dept
-where emp.DEPARTMENT_ID (+) = dept.DEPARTMENT_ID;   -- departments 테이블 레코드 전부를 출력에 참여 122 레코드
+from EMPLOYEES emp,
+     DEPARTMENTS dept
+where emp.DEPARTMENT_ID (+) = dept.DEPARTMENT_ID;
+-- departments 테이블 레코드 전부를 출력에 참여 122 레코드
 
 -- ANSI SQL
 select FIRST_NAME,
@@ -105,8 +118,8 @@ select FIRST_NAME,
        dept.DEPARTMENT_ID,
        DEPARTMENT_NAME
 from EMPLOYEES emp
-    right outer join DEPARTMENTS dept
-        on emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;
+         right outer join DEPARTMENTS dept
+                          on emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;
 
 
 --------------------
@@ -120,19 +133,23 @@ select FIRST_NAME,
        emp.DEPARTMENT_ID,
        dept.DEPARTMENT_ID,
        DEPARTMENT_NAME
-from EMPLOYEES emp full outer join DEPARTMENTS dept
-on emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;
+from EMPLOYEES emp
+         full outer join DEPARTMENTS dept
+                         on emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;
 
 --------------------
 -- NATURAL JOIN
 --------------------
 -- JOIN 할 테이블에 같은 이름의 컬럼이 있는 경우, 해당 컬럼을 기준으로 JOIN
 
-select * from EMPLOYEES emp natural join DEPARTMENTS dept;
+select *
+from EMPLOYEES emp
+         natural join DEPARTMENTS dept;
 -- select * from EMPLOYEES emp join DEPARTMENTS dept on emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;
 -- select * from EMPLOYEES emp join DEPARTMENTS dept on emp.MANAGER_ID = dept.MANAGER_ID;
 select *
-from EMPLOYEES emp join DEPARTMENTS dept on emp.MANAGER_ID = dept.MANAGER_ID and emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;
+from EMPLOYEES emp
+         join DEPARTMENTS dept on emp.MANAGER_ID = dept.MANAGER_ID and emp.DEPARTMENT_ID = dept.DEPARTMENT_ID;
 
 --------------------
 -- self join
@@ -140,15 +157,18 @@ from EMPLOYEES emp join DEPARTMENTS dept on emp.MANAGER_ID = dept.MANAGER_ID and
 -- 자기 자신과의 JOIN
 -- 자기자신을 두 번 호출 하기 때문에 -> 별칭을 반드시 부여해야 할 필요가 있는 JOIN
 
-select * from EMPLOYEES;        -- 107
+select *
+from EMPLOYEES; -- 107
 select emp.EMPLOYEE_ID,
        emp.FIRST_NAME,
        emp.MANAGER_ID,
        man.FIRST_NAME
 -- from EMPLOYEES emp join EMPLOYEES man
 -- on emp.MANAGER_ID = man.EMPLOYEE_ID;
-from EMPLOYEES emp, EMPLOYEES man
-where emp.MANAGER_ID = man.EMPLOYEE_ID; -- 106
+from EMPLOYEES emp,
+     EMPLOYEES man
+where emp.MANAGER_ID = man.EMPLOYEE_ID;
+-- 106
 
 -- 해봅시다. steven (매니저 없는 분까지)
 select emp.EMPLOYEE_ID,
@@ -157,8 +177,9 @@ select emp.EMPLOYEE_ID,
        man.FIRST_NAME
 -- from EMPLOYEES emp, EMPLOYEES man
 -- where emp.MANAGER_ID = man.EMPLOYEE_ID(+);
-from EMPLOYEES emp left outer join EMPLOYEES man
-on emp.MANAGER_ID = man.EMPLOYEE_ID;
+from EMPLOYEES emp
+         left outer join EMPLOYEES man
+                         on emp.MANAGER_ID = man.EMPLOYEE_ID;
 
 --------------------
 --Group Aggregation
@@ -168,38 +189,48 @@ on emp.MANAGER_ID = man.EMPLOYEE_ID;
 -- count : 갯수 세기 함수
 -- employees 테이블의 총 레코드 갯수
 
-select count(*) from EMPLOYEES;  -- 107
+select count(*)
+from EMPLOYEES;
+-- 107
 -- * 로 카운트 하면 모든 행의 수를 반환
 -- 특정 컬럼내에 null 값이 포함이 되어 있는지에 대한 여부는 중요하지 않음ㄴ
 
 -- commission 을 받는 직원의 수를 알고 싶을 경우
-select count(COMMISSION_PCT) from EMPLOYEES;    -- 35
+select count(COMMISSION_PCT)
+from EMPLOYEES;
+-- 35
 -- commission_pct 가 null인 경우를 제외하고 싶을 경우
-select count(*) from EMPLOYEES
+select count(*)
+from EMPLOYEES
 where COMMISSION_PCT is not null;
 
 -- sum : 합계 함수
 -- 모든 사원의 급여의 합
-select sum(SALARY) from EMPLOYEES;
+select sum(SALARY)
+from EMPLOYEES;
 
 -- AVG : 평균 함수
 -- 사원들의 평균 급여
-select avg(SALARY) from EMPLOYEES;
+select avg(SALARY)
+from EMPLOYEES;
 
 -- 사원들이 받는 커미션 비율의 평균
-select avg(COMMISSION_PCT) from EMPLOYEES;      -- 22%
+select avg(COMMISSION_PCT)
+from EMPLOYEES;
+-- 22%
 
 -- avg 함수는 null 값이 포함되어 있을 경우 그 값을 집계 수치에서 제외한다.
 -- null 값을 집계 결과에 포함시킬지의 여부는 정책으로 결정하고 수행해야 한다.
-select avg(nvl(COMMISSION_PCT, 0)) from EMPLOYEES;      -- 7%
+select avg(nvl(COMMISSION_PCT, 0))
+from EMPLOYEES;
+-- 7%
 
 -- min / max 함수 : 최소값 & 최대값
 --avg / median : 산술평균 / 중앙값(중요한 통계수치)
-select
-    min(SALARY) 최소급여,
-    max(SALARY) 최대급여,
-    avg(SALARY) 평균급여,
-    median(SALARY) 급여중앙값
+select min(SALARY)    최소급여,
+       max(SALARY)    최대급여,
+       avg(SALARY)    평균급여,
+       median(SALARY) 급여중앙값
 from EMPLOYEES;
 
 -- 흔히 범하는 오류..
@@ -209,8 +240,10 @@ select DEPARTMENT_ID, avg(SALARY)   -- 오류 : 단일 그룹이 아님
 from EMPLOYEES;
 */
 
-select DEPARTMENT_ID from EMPLOYEES;    -- 여러 개의 레코드
-select avg(SALARY) from EMPLOYEES;      -- 단일 레코드
+select DEPARTMENT_ID
+from EMPLOYEES; -- 여러 개의 레코드
+select avg(SALARY)
+from EMPLOYEES; -- 단일 레코드
 
 select DEPARTMENT_ID, SALARY
 from EMPLOYEES
@@ -219,5 +252,110 @@ order by DEPARTMENT_ID;
 -- GROUP BY
 select DEPARTMENT_ID, ROUND(avg(SALARY), 2)
 from EMPLOYEES
-group by DEPARTMENT_ID      -- 집계를 위해 특정 컬럼을 기준으로 그룹핑 한다.
+group by DEPARTMENT_ID -- 집계를 위해 특정 컬럼을 기준으로 그룹핑 한다.
 order by DEPARTMENT_ID;
+
+-- 부서별 평균 급여에 부서명도 포함하여 출력
+select e.DEPARTMENT_ID,
+       d.DEPARTMENT_NAME,
+       ROUND(avg(e.SALARY), 2)
+from EMPLOYEES e
+         join DEPARTMENTS d on e.DEPARTMENT_ID = d.DEPARTMENT_ID
+GROUP BY e.DEPARTMENT_ID, d.DEPARTMENT_NAME
+order by e.DEPARTMENT_ID;
+
+-- Group by 절 이후에는 Group by 에 참여한 컬럼과 집계 함수만 남는다.
+
+-- 평균 급여가 7000 이상인 부서만 출력
+select DEPARTMENT_ID,
+       ROUND(avg(SALARY), 2) 평균급여
+from EMPLOYEES
+where avg(SALARY) >= 7000 -- 아직까지 집계 함수가 시행되지 않은 상태 => 집계 함수의 비교는 불가하다.
+group by DEPARTMENT_ID
+order by DEPARTMENT_ID;
+
+-- 집계 함수 이후의 조건 비교는 Having 절 이용 해야함
+select DEPARTMENT_ID,
+       ROUND(avg(SALARY), 2)
+from EMPLOYEES
+group by DEPARTMENT_ID
+having avg(SALARY) >= 7000 -- Group by 와  aggregation 의 조건 필터링은 Having 절에서
+order by DEPARTMENT_ID;
+
+-- ROll UP 함수
+-- GROUP BY 절과 함께 사용
+-- 그룸 지어진 결과에 대한 좀 더 상세한 요약을 제공함.
+-- 일종의 Item TOTAL 값을 집계해 주는 함수
+select DEPARTMENT_ID,
+       JOB_ID,
+       sum(SALARY)
+from EMPLOYEES
+group by rollup (DEPARTMENT_ID, JOB_ID);
+
+
+--CUBE
+-- crossTab 에 대한 Summary 를 함께 추출하는 함수
+-- Rollup 함수에 의해 출력되는 Item total 값과 column total 값을 함께 추출
+select DEPARTMENT_ID,
+       JOB_ID,
+       sum(SALARY)
+from EMPLOYEES
+group by cube (DEPARTMENT_ID, JOB_ID)
+order by DEPARTMENT_ID;
+
+-----------------
+-- subquery
+-----------------
+
+-- 모든 직원 급여의 중앙값보다 많은 급여를 받는 사원은?
+-- 1) 모든 직원 급여의 중앙값은?
+-- 2) 1)번의 결과보다 많은 급여를 받는 직원의 목록?
+-- 1. 직원 급여 중앙값
+select median(SALARY)
+from EMPLOYEES;
+-- 6200
+--2. 1번의 결과(6200)보다 많은 급여를 받는 직원목록
+select FIRST_NAME,
+       SALARY
+from EMPLOYEES
+where SALARY >= 6200;
+
+-- 1), 2) 쿼리 합치기
+select FIRST_NAME, SALARY
+from EMPLOYEES
+where SALARY >= (select median(SALARY)
+                 from EMPLOYEES)
+order by SALARY desc;
+
+-- susan 보다 늦게 입사한 사원의 정보는?
+-- 1) Susan 의 입사일
+-- 2) 1)번의 결과보다 늦게 입사한 사원의 정보 뽑기
+
+-- 1. 수잔의 입사일
+select HIRE_DATE
+from EMPLOYEES
+where upper(FIRST_NAME) = 'SUSAN';
+-- 12/06/07
+
+-- 2. 1) 결과(12/06/07) 보다 늦게 입사한 사원 정보
+select FIRST_NAME, HIRE_DATE
+from EMPLOYEES
+where HIRE_DATE > '12/06/07';
+
+-- 1) ,2) 쿼리 합치기
+select FIRST_NAME, HIRE_DATE
+from EMPLOYEES
+where HIRE_DATE > (select HIRE_DATE
+                   from EMPLOYEES
+                   where UPPER(FIRST_NAME) = 'SUSAN');
+
+
+-- 연습문제
+-- 모든 직원 급여의 중앙값 보다 많이 받으면서 수잔보다 입사일이 늦은 사람
+select FIRST_NAME, SALARY, HIRE_DATE
+from EMPLOYEES
+where SALARY >= (select median(SALARY)
+                 from EMPLOYEES) and HIRE_DATE > (select HIRE_DATE
+                   from EMPLOYEES
+                   where UPPER(FIRST_NAME) = 'SUSAN')
+order by HIRE_DATE asc, SALARY desc ;
--- PRACTICE02.sql
+++ PRACTICE02.sql
@@ -8,8 +8,8 @@ WHERE manager_id IS NOT NULL;
 -- 문제2.
 -- 직원중에 최고임금(salary)과 최저임금을 “최고임금, “최저임금”프로젝션 타이틀로 함께 출력해 보세요.
 -- 두 임금의 차이는 얼마인가요? “최고임금 – 최저임금”이란 타이틀로 함께 출력해 보세요.
-SELECT MAX(salary) AS "최고임금",
-       MIN(salary) AS "최저임금",
+SELECT MAX(salary)               AS "최고임금",
+       MIN(salary)               AS "최저임금",
        MAX(salary) - MIN(salary) AS "최고임금 - 최저임금"
 FROM employees;
 
@@ -25,10 +25,10 @@ FROM EMPLOYEES;
 -- 문제4.
 -- 부서별로 평균임금, 최고임금, 최저임금을 부서아이디(department_id)와 함께 출력합니다.
 -- 정렬순서는 부서번호(department_id) 내림차순입니다.
-SELECT  DEPARTMENT_ID,
-          ROUND(AVG(SALARY), 2) AS 평균임금,
-          MAX(SALARY) AS 최고임금,
-          MIN(SALARY) AS 최저임금
+SELECT DEPARTMENT_ID,
+       ROUND(AVG(SALARY), 2) AS 평균임금,
+       MAX(SALARY)           AS 최고임금,
+       MIN(SALARY)           AS 최저임금
 FROM EMPLOYEES
 group by DEPARTMENT_ID
 order by DEPARTMENT_ID desc;
@@ -40,8 +40,8 @@ order by DEPARTMENT_ID desc;
 -- (정렬순서는 최소임금 2500 구간일때 확인해볼 것)
 SELECT JOB_ID,
        ROUND(AVG(SALARY), 0) AS 평균임금,
-       MAX(SALARY) AS 최고임금,
-       MIN(SALARY) AS 최저임금
+       MAX(SALARY)           AS 최고임금,
+       MIN(SALARY)           AS 최저임금
 FROM EMPLOYEES
 group by JOB_ID
 ORDER BY 최저임금 desc, 평균임금 asc, JOB_ID asc;
@@ -57,8 +57,8 @@ FROM EMPLOYEES;
 -- 문제7.
 -- 평균임금과 최저임금의 차이가 2000 미만인 부서(department_id), 평균임금, 최저임금 그리고 (평균임금 – 최저임금)를 (평균임금 – 최저임금)의 내림차순으로 정렬해서 출력하세요.
 select DEPARTMENT_ID,
-       avg(SALARY) as 평균임금,
-       min(SALARY) as 최저임금,
+       avg(SALARY)               as 평균임금,
+       min(SALARY)               as 최저임금,
        avg(SALARY) - min(SALARY) as "(평균임금 - 최저임금)"
 from EMPLOYEES
 group by DEPARTMENT_ID
@@ -69,11 +69,12 @@ order by (평균임금 - 최저임금) desc;
 -- 문제8.
 -- 업무(JOBS)별로 최고임금과 최저임금의 차이를 출력해보세요.
 -- 차이를 확인할 수 있도록 내림차순으로 정렬하세요?
-select JOB_TITLE, MIN_SALARY, MAX_SALARY,
+select JOB_TITLE,
+       MIN_SALARY,
+       MAX_SALARY,
        MAX_SALARY - MIN_SALARY AS 임금차이
 FROM JOBS
 ORDER BY 임금차이 desc;
-
 
 
 
@@ -82,9 +83,9 @@ ORDER BY 임금차이 desc;
 -- 출력은 관리자별로 평균급여가 5000이상 중에 평균급여 최소급여 최대급여를 출력합니다.
 -- 평균급여의 내림차순으로 정렬하고 평균급여는 소수점 첫째짜리에서 반올림 하여 출력합니다.
 SELECT MANAGER_ID,
-    Round(avg(SALARY),1) as 평균급여,
-    max(SALARY) as 최대급여,
-    min(SALARY) as 최소급여
+       Round(avg(SALARY), 1) as 평균급여,
+       min(SALARY)           as 최소급여,
+       max(SALARY)           as 최대급여
 FROM EMPLOYEES
 WHERE HIRE_DATE > TO_DATE('2015-01-01', 'YYYY-MM-DD')
 GROUP BY MANAGER_ID
@@ -92,8 +93,23 @@ HAVING avg(SALARY) >= 5000
 ORDER BY 평균급여 desc;
 
 
--- 문제10
+--문제10
 -- 아래회사는 보너스 지급을 위해 직원을 입사일 기준으로 나눌려고 합니다.
--- 입사일이 02/12/31일 이전이면 '창립맴버, 03년은 '03년입사’, 04년은 ‘04년입사’
+-- 입사일이 12/12/31일 이전이면 '창립맴버, 13년은 '13년입사’, 14년은 ‘14년입사’
 -- 이후입사자는 ‘상장이후입사’ optDate 컬럼의 데이터로 출력하세요.
 -- 정렬은 입사일로 오름차순으로 정렬합니다.
+
+select EMPLOYEE_ID,       -- 직원의 고유 식별자
+       FIRST_NAME,        -- 직원의 이름
+       LAST_NAME,         -- 직원의 성
+       HIRE_DATE,         -- 직원이 회사에 입사한 날짜
+       case
+           when HIRE_DATE < TO_DATE('2013-01-01', 'YYYY-MM-DD')
+               then '창립맴버' -- -- TO_DATE 함수를 사용하여 문자열을 날짜 타입으로 변환. 날짜 비교를 통해 2003년 이전에 입사한 직원들을 창립멤버로 분류함.
+           when TO_CHAR(hire_date, 'YYYY') = '2013' then '13년입사' -- TO_CHAR 함수를 사용하여 날짜에서 연도를 문자열로 추출.
+           when TO_CHAR(hire_date, 'YYYY') = '2014' then '14년입사' -- 추출된 연도가 '2004'인 경우 해당 직원을 '04년입사'로 분류함.
+           else '상장이후입사' -- 위의 모든 조건에 해당하지 않는 경우, 직원을 '상장이후입사'로 분류함.
+           end AS optDate -- CASE 문을 사용해 계산된 값을 optDate 라는 새로운 열로 표시
+from EMPLOYEES -- employees 테이블에서 데이터를 선택함
+order by HIRE_DATE ASC; -- hire_date 기준으로 결과를 오름차순으로 정렬함
+
